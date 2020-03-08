23803b827267d67fd425175a5f1be9234b4ee5a8
Rishi <rushichaudhari26@gmail.com>
2020-03-08 18:41:49 +0530

    requirement file added

--- saptshrungi/settings.py
+++ saptshrungi/settings.py
@@ -132,11 +132,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = ''
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = ( os.path.join('static'), )
 
 # Added Auth for Abstract User
 AUTH_USER_MODEL = "user.User"
