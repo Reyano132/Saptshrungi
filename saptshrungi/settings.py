eebf33e0507c0181852205cd82f80249f9f3c818
Rishi <rushichaudhari26@gmail.com>
2020-03-08 19:20:21 +0530

    Procfile added

--- saptshrungi/settings.py
+++ saptshrungi/settings.py
@@ -132,7 +132,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles_')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
